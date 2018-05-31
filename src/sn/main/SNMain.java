20241729eae77d5f2fd37ada140d2989a5c030a1
Man Nguyen <n2m@debian>
2018-05-31 08:07:51 +0700

    add NotReverse

--- src/sn/main/SNMain.java
+++ src/sn/main/SNMain.java
@@ -3,6 +3,7 @@ package sn.main;
 import java.util.Scanner;
 
 import sn.sumnumber.MyBigNumber;
+import sn.sumnumber.NotReverse;
 
 public class SNMain {
 
@@ -13,7 +14,8 @@ public class SNMain {
 		String s1=sc.nextLine();
 		System.out.print("Nhap chuoi so s2: ");
 		String s2=sc.nextLine();
-		MyBigNumber mbn = new MyBigNumber();
+		//MyBigNumber mbn = new MyBigNumber();
+		NotReverse mbn = new NotReverse();
 		System.out.println("Tong cua chuoi "+s1+" va "+s2+" la "+mbn.sum(s1, s2));
 		
 	}
